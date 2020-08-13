9082e02f731940a55a9e5010ffacb94d2fa7ad11
vitalii <enjoylifethemost123@gmail.com>
2020-08-13 19:41:15 +0300

    public

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // This is necessary middleware to parse JSON into the incoming request body for POST requests
@@ -10,12 +10,17 @@ app.use(bodyParser.json());
  * - CDS Services must implement CORS in order to be called from a web browser
  */
 app.use((request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  response.setHeader('Access-Control-Allow-Credentials', 'true');
-  response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  response.setHeader('Access-Control-Expose-Headers', 'Origin, Accept, Content-Location, ' +
-    'Location, X-Requested-With');
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  response.setHeader(
+    "Access-Control-Expose-Headers",
+    "Origin, Accept, Content-Location, " + "Location, X-Requested-With"
+  );
 
   // Pass to next layer of middleware
   next();
@@ -27,32 +32,38 @@ app.use((request, response, next) => {
  */
 app.use((request, response, next) => {
   // Always allow OPTIONS requests as part of CORS pre-flight support.
-  if (request.method === 'OPTIONS') {
+  if (request.method === "OPTIONS") {
     next();
     return;
   }
 
-  const serviceHost = request.get('Host');
-  const authorizationHeader = request.get('Authorization') || 'Bearer open'; // Default a token until ready to enable auth.
+  const serviceHost = request.get("Host");
+  const authorizationHeader = request.get("Authorization") || "Bearer open"; // Default a token until ready to enable auth.
 
-  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer')) {
-    response.set('WWW-Authenticate', `Bearer realm="${serviceHost}", error="invalid_token", error_description="No Bearer token provided."`)
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer")) {
+    response.set(
+      "WWW-Authenticate",
+      `Bearer realm="${serviceHost}", error="invalid_token", error_description="No Bearer token provided."`
+    );
     return response.status(401).end();
   }
 
-  const token = authorizationHeader.replace('Bearer ', '');
+  const token = authorizationHeader.replace("Bearer ", "");
   const aud = `${request.protocol}://${serviceHost}${request.originalUrl}`;
 
   const isValid = true; // Verify token validity per https://cds-hooks.org/specification/current/#trusting-cds-clients
 
   if (!isValid) {
-    response.set('WWW-Authenticate', `Bearer realm="${serviceHost}", error="invalid_token", error_description="The token is invalid."`)
+    response.set(
+      "WWW-Authenticate",
+      `Bearer realm="${serviceHost}", error="invalid_token", error_description="The token is invalid."`
+    );
     return response.status(401).end();
   }
 
   // Pass to next layer of middleware
   next();
-})
+});
 
 /**
  * Discovery Endpoint:
@@ -60,31 +71,30 @@ app.use((request, response, next) => {
  * - This function should respond with definitions of each CDS Service for this app in JSON format
  * - See details here: https://cds-hooks.org/specification/current/#discovery
  */
-app.get('/cds-services', (request, response) => {
-
+app.get("/cds-services", (request, response) => {
   // Example service to invoke the patient-view hook
   const patientViewExample = {
-    hook: 'patient-view',
-    id: 'patient-view-example',
-    title: 'Example patient-view CDS Service',
-    description: 'Displays the name of the patient',
+    hook: "patient-view",
+    id: "patient-view-example",
+    title: "Example patient-view CDS Service",
+    description: "Displays the name of the patient",
     prefetch: {
       // Request the Patient FHIR resource for the patient in context, where the EHR fills out the prefetch template
       // See details here: https://cds-hooks.org/specification/current/#prefetch-template
-      requestedPatient: 'Patient/{{context.patientId}}'
-    }
+      requestedPatient: "Patient/{{context.patientId}}",
+    },
   };
 
   // Example service to invoke the order-select hook
   const orderSelectExample = {
-    hook: 'order-select',
-    id: 'order-select-example',
-    title: 'Example order-select CDS Service',
-    description: 'Suggests prescribing Aspirin 81 MG Oral Tablets',
+    hook: "order-select",
+    id: "order-select-example",
+    title: "Example order-select CDS Service",
+    description: "Suggests prescribing Aspirin 81 MG Oral Tablets",
   };
 
   const discoveryEndpointServices = {
-    services: [ patientViewExample, orderSelectExample ]
+    services: [patientViewExample, orderSelectExample],
   };
   response.send(JSON.stringify(discoveryEndpointServices, null, 2));
 });
@@ -96,29 +106,33 @@ app.get('/cds-services', (request, response) => {
  *
  * - Service purpose: Display a patient's first and last name, with a link to the CDS Hooks web page
  */
-app.post('/cds-services/patient-view-example', (request, response) => {
-
+app.post("/cds-services/patient-view-example", (request, response) => {
   // Parse the request body for the Patient prefetch resource
   const patientResource = request.body.prefetch.requestedPatient;
   const patientViewCard = {
     cards: [
       {
         // Use the patient's First and Last name
-        summary: 'Now seeing: ' + patientResource.name[0].given[0] + ' ' + patientResource.name[0].family[0],
-        indicator: 'info',
+        summary:
+          "Now seeing: " +
+          patientResource.name[0].given[0] +
+          " " +
+          patientResource.name[0].family[0],
+        indicator: "info",
         source: {
-          label: 'CDS Service Tutorial',
-          url: 'https://github.com/cerner/cds-services-tutorial/wiki/Patient-View-Service'
+          label: "CDS Service Tutorial",
+          url:
+            "https://github.com/cerner/cds-services-tutorial/wiki/Patient-View-Service",
         },
         links: [
           {
-            label: 'Learn more about CDS Hooks',
-            url: 'https://cds-hooks.org',
-            type: 'absolute'
-          }
-        ]
-      }
-    ]
+            label: "Learn more about CDS Hooks",
+            url: "https://cds-hooks.org",
+            type: "absolute",
+          },
+        ],
+      },
+    ],
   };
   response.send(JSON.stringify(patientViewCard, null, 2));
 });
@@ -132,15 +146,19 @@ app.post('/cds-services/patient-view-example', (request, response) => {
  *                    provider to change their chosen medication to the service-recommended Aspirin 81 MG Oral Tablet,
  *                    or display text that affirms the provider is currently prescribing the service-recommended Aspirin
  */
-app.post('/cds-services/order-select-example', (request, response) => {
-
+app.post("/cds-services/order-select-example", (request, response) => {
   // Parse the request body for the FHIR context provided by the EHR. In this case, the MedicationRequest/MedicationOrder resource
   const draftOrder = request.body.context.draftOrders.entry[0].resource;
   const selections = request.body.context.selections;
 
   // Check if a medication was chosen by the provider to be ordered
-  if (['MedicationRequest', 'MedicationOrder'].includes(draftOrder.resourceType) && selections.includes(`${draftOrder.resourceType}/${draftOrder.id}`)
-    && draftOrder.medicationCodeableConcept) {
+  if (
+    ["MedicationRequest", "MedicationOrder"].includes(
+      draftOrder.resourceType
+    ) &&
+    selections.includes(`${draftOrder.resourceType}/${draftOrder.id}`) &&
+    draftOrder.medicationCodeableConcept
+  ) {
     const responseCard = createMedicationResponseCard(draftOrder); // see function below for more details
     response.send(JSON.stringify(responseCard, null, 2));
   }
@@ -153,23 +171,25 @@ app.post('/cds-services/order-select-example', (request, response) => {
  * @returns {{cards: *[]}} - Either a card with the suggestion to switch medication or a textual info card
  */
 function createMedicationResponseCard(context) {
-  const providerOrderedMedication = context.medicationCodeableConcept.coding[0].code;
+  const providerOrderedMedication =
+    context.medicationCodeableConcept.coding[0].code;
 
   // Check if medication being ordered is our recommended Aspirin 81 MG Oral Tablet
-  if (providerOrderedMedication === '243670') {
+  if (providerOrderedMedication === "243670") {
     // Return this card if the provider has already chosen this specific medication to prescribe,
     // or the provider has chosen the suggestion to switch to this specific medication already
     return {
       cards: [
         {
-          summary: 'Currently prescribing a low-dose Aspirin',
-          indicator: 'info',
+          summary: "Currently prescribing a low-dose Aspirin",
+          indicator: "info",
           source: {
-            label: 'CDS Service Tutorial',
-            url: 'https://github.com/cerner/cds-services-tutorial/wiki/Order-Select-Service'
-          }
-        }
-      ]
+            label: "CDS Service Tutorial",
+            url:
+              "https://github.com/cerner/cds-services-tutorial/wiki/Order-Select-Service",
+          },
+        },
+      ],
     };
   } else {
     // 1. Copy the current MedicationRequest/MedicationOrder resource the provider intends to prescribe
@@ -178,39 +198,41 @@ function createMedicationResponseCard(context) {
     //    copy instead, if the provider clicks on the suggestion button
     let newMedicationRequest = context;
     newMedicationRequest.medicationCodeableConcept = {
-      text: 'Aspirin 81 MG Oral Tablet',
+      text: "Aspirin 81 MG Oral Tablet",
       coding: [
         {
-          display: 'Aspirin 81 MG Oral Tablet',
-          system: 'http://www.nlm.nih.gov/research/umls/rxnorm',
-          code: '243670'
-        }
-      ]
+          display: "Aspirin 81 MG Oral Tablet",
+          system: "http://www.nlm.nih.gov/research/umls/rxnorm",
+          code: "243670",
+        },
+      ],
     };
 
     return {
       cards: [
         {
-          summary: 'Reduce cardiovascular risks, prescribe daily 81 MG Aspirin',
-          indicator: 'warning',
+          summary: "Reduce cardiovascular risks, prescribe daily 81 MG Aspirin",
+          indicator: "warning",
           suggestions: [
             {
-              label: 'Switch to low-dose Aspirin',
+              label: "Switch to low-dose Aspirin",
               actions: [
                 {
-                  type: 'create',
-                  description: 'Modifying existing medication order to be Aspirin',
-                  resource: newMedicationRequest
-                }
-              ]
-            }
+                  type: "create",
+                  description:
+                    "Modifying existing medication order to be Aspirin",
+                  resource: newMedicationRequest,
+                },
+              ],
+            },
           ],
           source: {
-            label: 'CDS Service Tutorial',
-            url: 'https://github.com/cerner/cds-services-tutorial/wiki/Order-Select-Service'
-          }
-        }
-      ]
+            label: "CDS Service Tutorial",
+            url:
+              "https://github.com/cerner/cds-services-tutorial/wiki/Order-Select-Service",
+          },
+        },
+      ],
     };
   }
 }
